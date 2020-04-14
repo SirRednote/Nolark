e8b738a88664f7dafd4f759febf52f10967b6210
José GIL <jgil83000@gmail.com>
2020-04-14 13:16:57 +0200

    TP 11 + 12 fini : installation de la base de données, correspondance avec le projet Nolark + installation du package

--- includes/header.html.inc.php
+++ includes/header.html.inc.php
@@ -1,26 +1,48 @@
-<picture>
-    <source media="(max-width: 576px)" srcset="images/banniere_small.png">
-    <source srcset="images/banniere.png">
-    <img src="images/banniere.png" alt="Nolark : Protect your minds ! Cette bannière montre un 
-    coucher de soleil avec une femme embrassant un homme réalisant en stoppie sur sa
-    moto.">
-    <!-- Image basée sur la création originale de ShiftGraphiX sur Pixabay : 
-    https://pixabay.com/fr/couple-stoppie-sportive-vélomoteur-3156613/ -->
-</picture>
-<nav>
-    <input type="checkbox">
+<?php
+    // Sommes-nous sur l'index ? RÃ©cupÃ©ration du nom de page dans $pageActuelle
+    $scriptName = filter_input(INPUT_SERVER, 'SCRIPT_NAME');
+    $pageActuelle = substr($scriptName, strrpos($scriptName, '/') + 1);
+    if ($pageActuelle === 'index.php') {
+        $dirIndex = './';
+        $dirPages = './pages/';
+    } else {
+        $dirIndex = '../';
+        $dirPages = './';
+    }
+    // Construction d'un tableau associatif contenant les correspondances
+    // noms de pages / liens de la barre de navigation
+    $pages = array(
+        'Accueil' => $dirIndex . 'index.php',
+        'Route' => $dirPages . 'route.php',
+        'Cross' => $dirPages . 'cross.php',
+        'Piste' => $dirPages . 'piste.php',
+        'Enfants' => $dirPages . 'enfants.php',
+        'Nous contacter' => $dirPages . 'nous-contacter.php'
+    );
+?>
+
+<header>
+    <picture>
+        <source media="(max-width: 576px)" srcset="<?php echo $dirIndex; ?>images/banniere_small.png">
+        <source srcset="<?php echo $dirIndex; ?>images/banniere.png">
+        <img src="<?php echo $dirIndex; ?>images/banniere.png" alt="Nolark : Protect your minds ! Cette banniÃ¨re montre un 
+             coucher de soleil avec une femme embrassant un homme rÃ©alisant en stoppie sur sa
+             moto.">
+        <!-- Image basÃ©e sur la crÃ©ation originale de ShiftGraphiX sur Pixabay : 
+            https://pixabay.com/fr/couple-stoppie-sportive-vÃ©lomoteur-3156613/ -->
+    </picture>
+    <nav>
+        <input type="checkbox">
         <span></span>
         <span></span>
         <span></span>
         <ul>
-            <li><a href="index.php">Accueil</a></li>
             <?php
-                $tabAssociatif = array("Route" => "route.php", "Cross" => "cross.php", "Piste" => 
-                "piste.php", "Enfants" => "enfants.php", "Nous contacter" => "nous-contacter.php");
-
-                foreach ($tabAssociatif as $nom => $url) {
-                    echo '<li><a href="pages/', $url, '">', $nom, '</a></li>';
-                }
+            // Affichage des liens de la barre de navigation
+            foreach ($pages as $nom => $url) {
+                echo "\n", '<li><a href="', $url, '">', $nom, '</a></li>';
+            }
             ?>
         </ul>
-</nav>
+    </nav>
+</header>
--- pages/simulateur.php
+++ pages/simulateur.php
@@ -8,7 +8,7 @@ and open the template in the editor.
     <head>
         <title>Casques Nolark : Sécurité et confort, nos priorités !</title>
         <meta charset="UTF-8">
-        <meta name="author" content="Maxime TEILLAUD">
+        <meta name="author" content="José GIL">
         <meta name="description" content="Découvrez des casques moto dépassant 
         même les exigences des tests de sécurité. Tous les casques Nolark au 
         meilleur prix et avec en prime la livraison gratuite !">
